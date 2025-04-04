81796d874c7ad24cc9368cc57099d9bb94ec8745
andre <andre.herrera@unah.hn>
2025-04-04 08:13:13 -0600

    3 commit

--- db.tf
+++ db.tf
@@ -40,3 +40,27 @@ resource "azurerm_search_service" "search" {
   
 }
 
+resource "azurerm_cosmosdb_account" "mongo" {
+  name = "cosmosdb2-${var.project_name}-${var.environment}"
+  location = var.location
+  resource_group_name = "rg-${var.project_name}-${var.environment}"
+  offer_type = "Standard"
+  kind = "MongoDB"
+  geo_location {
+    location          = var.location
+    failover_priority = 0
+  }
+  consistency_policy {
+    consistency_level = "Session"
+  }
+  tags = var.tags
+  capabilities {
+    name = "EnableMongo"
+  }
+}
+
+resource "azurerm_cosmosdb_mongo_database" "mongo_db" {
+  name = "cosmosdb2-${var.project_name}-${var.environment}"
+  resource_group_name = "rg-${var.project_name}-${var.environment}"
+  account_name = azurerm_cosmosdb_account.mongo.name
+}
